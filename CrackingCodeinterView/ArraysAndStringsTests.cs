09e179172ba90afeefe32aa74b09bc1434cd3e10
garryto <aldonapuri@gmail.com>
2017-06-04 22:17:01 -0500

    Nuevos

---
+++ CrackingCodeinterView/ArraysAndStringsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace CrackingCodeinterView
+{
+    public static class ArraysAndStringsSolutions
+    {
+        /// <summary>
+        /// 1.1 Checks if a string has unique characters.
+        /// </summary>
+        /// <returns><c>true</c>, if unique string was ised, <c>false</c> otherwise.</returns>
+        /// <param name="strVal">String value.</param>
+		//Assuming 'a to z' values of the string        
+		public static bool isUniqueString(string strVal)
+		{
+			int checker = 0;
+			for (int i = 0; i < strVal.Length; i++)
+			{
+				int val = strVal[i] - 'a';
+				if ((checker & (1 << val)) > 0)
+				{
+
+					return false;
+				}
+				checker |= (1 << val);
+
+			}
+
+			return true;
+
+		}
+    }
+}
---
+++ CrackingCodeinterView/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System;
+
+namespace CrackingCodeinterView
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			// Arrays And Strings Cracking The Code Interview
+
+			string strValor = "Aldo";
+
+			bool result = ArraysAndStringsSolutions.isUniqueString(strValor);
+
+			if (result)
+				Console.WriteLine("true");
+			else
+				Console.WriteLine("false");
+        }
+
+		
+    }
+}
+
+
